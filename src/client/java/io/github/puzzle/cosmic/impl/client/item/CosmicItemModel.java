3c883aecd9b403a41a3c3bdffca08384128b1367
Mr_Zombii <thezombiigames@gmail.com>
2025-12-02 16:10:52 -0600

    Remove redundant logger

--- src/client/java/io/github/puzzle/cosmic/impl/client/item/CosmicItemModel.java
+++ src/client/java/io/github/puzzle/cosmic/impl/client/item/CosmicItemModel.java
@@ -32,8 +32,6 @@ import io.github.puzzle.cosmic.impl.data.point.single.IdentifierDataPoint;
 import io.github.puzzle.cosmic.impl.data.point.single.PairDataPoint;
 import io.github.puzzle.cosmic.item.AbstractCosmicItem;
 import io.github.puzzle.cosmic.item.ItemDataPointSpecs;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -45,7 +43,6 @@ import static finalforeach.cosmicreach.rendering.items.ItemRenderer.registerItem
 
 public class CosmicItemModel implements ICosmicItemModel {
 
-    private static final Logger log = LoggerFactory.getLogger(CosmicItemModel.class);
     static HashMap<String, Texture> ITEM_TEXTURE_CACHE = new HashMap<>();
     static HashMap<String, Mesh> ITEM_MESH_CACHE = new HashMap<>();
     static HashMap<String, Pair<String, String>> INDEX_CACHE = new HashMap<>();
