365bbb800f4d1b61fcabbe71ab2aba014be19f72
DarkMattrMaestro <94948678+DarkMattrMaestro@users.noreply.github.com>
2026-07-06 13:15:33 -0400

    Update to CR0.5.25
    Update version and fix mixin that broke in the CR0.5.25

--- src/common/java/io/github/puzzle/cosmic/impl/mixin/singletons/GameSingletonMixin.java
+++ src/common/java/io/github/puzzle/cosmic/impl/mixin/singletons/GameSingletonMixin.java
@@ -13,7 +13,7 @@ import static finalforeach.cosmicreach.singletons.GameSingletons.loadingQueue;
 @Mixin(GameSingletons.class)
 public class GameSingletonMixin {
 
-    @Inject(method = "postCreate", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/items/recipes/CraftingRecipes;loadCraftingRecipes(Lcom/badlogic/gdx/utils/Queue;)V", shift = At.Shift.AFTER))
+    @Inject(method = "postCreate", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/items/recipes/RecipeRegistry;loadAll()V", shift = At.Shift.AFTER))
     private static void postCreate(CallbackInfo ci) {
         EventLoadingQueue event = new EventLoadingQueue(loadingQueue);
         GameRegistries.COSMIC_EVENT_BUS.post(event);
